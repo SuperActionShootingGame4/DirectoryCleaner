1b1ea9ebcf08f80dd85ec91fc32aaad0ffbf842f
nishihiro <48039371+SuperActionShootingGame4@users.noreply.github.com>
2019-05-01 09:57:41 +0900

    Update DirectoryCleaner.py
    ファイルリストをcsv出力する機能を追加

--- DirectoryCleaner.py
+++ DirectoryCleaner.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import csv
 from pathlib import Path
 import sqlite3
 from contextlib import closing
@@ -7,6 +8,12 @@ import hashlib
 from argparse import ArgumentParser
 
 dbname = 'test.db'
+
+def write_csv(list):
+	with open("C://test/abc.csv", 'a') as f:
+		writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
+		writer.writerow(list)     # list（1次元配列）の場合
+	return True
 
 #ファイルリスト作成
 def listup_files(path):
@@ -16,10 +23,9 @@ def listup_files(path):
 
 #md5計算
 def hash_md5(path):
-	f = open(path,"rb")
-	BinaryData = f.read()
-	f.close()
-	return hashlib.md5(BinaryData).hexdigest()
+	with closing(open(path, "rb")) as file:
+		binary = file.read()
+	return hashlib.md5(binary).hexdigest()
 
 #db作成
 def create_db(path, list):
@@ -35,7 +41,14 @@ def create_db(path, list):
 												file_time int, 
 												file_md5 int )'''
 		c.execute(create_table)
-		sql = "insert into flist (id, filea_name, filea_path, file_ext, file_size, file_time, file_md5) values (?,?,?,?,?,?,?)"
+		sql = '''insert into flist (id,
+									filea_name,
+									filea_path,
+									file_ext,
+									file_size,
+									file_time,
+									file_md5) values (?,?,?,?,?,?,?)'''
+		write_csv(["file list"])
 		for i in range(len(list)):
 			if list[i].is_file():
 				file_size = os.path.getsize(list[i])
@@ -45,7 +58,7 @@ def create_db(path, list):
 					flist = (i, list[i].name, str(list[i]), list[i].suffix, 
 							 file_size, datetime, md5)
 					c.execute(sql, flist)
-		
+					write_csv(flist)
 		check_duplicate(c)
 		#conn.close()
 	return
@@ -58,16 +71,17 @@ def check_duplicate(c):
 	print(val)
 	print(type(val))
 	print(val[0][6])
+	write_csv(["remove file list"])
 	for i in range(len(val)):
 		select_sql = "SELECT * FROM flist WHERE file_md5 = '%s' " % val[i][6]
 		c.execute(select_sql)
 		tmp = c.fetchall()
-		print("抽出したリスト\n")
-		print(tmp)
 		for j in range(1, len(tmp)):
 			if os.path.isfile(tmp[j][2]):
+				os.remove(tmp[j][2])
 				c.execute(u"delete from flist where id=%s" % tmp[j][0])
-				os.remove(tmp[j][2])				
+				write_csv(tmp[j])
+								
 	return
 
 def get_option():
